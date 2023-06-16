cf48c7553e16ed156007f0fb6514675f36e11f75
Henok Kirubel <henokhm2@gmail.com>
2023-06-16 22:27:58 +0300

    Run Webpack build

--- dist/main.js
+++ dist/main.js
@@ -763,7 +763,7 @@ class ToDoList {
 
   createToDoItem(description) {
     const newToDo = {
-      index: this.#toDoList.length,
+      index: this.#toDoList.length + 1,
       description,
       completed: false,
     };
@@ -788,7 +788,7 @@ class ToDoList {
     });
 
     this.#toDoList.forEach((toDoItem, index) => {
-      toDoItem.index = index;
+      toDoItem.index = index + 1;
     });
 
     localStorage.setItem('to-do-list', JSON.stringify(this.#toDoList));
@@ -942,6 +942,17 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
+/* harmony import */ var _assets_icons_tick_svg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../assets/icons/tick.svg */ "./src/assets/icons/tick.svg");
+/* harmony import */ var _assets_icons_trash_light_svg__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../assets/icons/trash-light.svg */ "./src/assets/icons/trash-light.svg");
+/* harmony import */ var _assets_icons_trash_svg__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../assets/icons/trash.svg */ "./src/assets/icons/trash.svg");
+/* harmony import */ var _assets_icons_dots_light_svg__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../assets/icons/dots-light.svg */ "./src/assets/icons/dots-light.svg");
+/* harmony import */ var _assets_icons_dots_svg__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../assets/icons/dots.svg */ "./src/assets/icons/dots.svg");
+
+
+
+
+
+
 const generateToDoItemHTML = (toDoItem) => {
   const li = document.createElement('li');
   const checkBox = document.createElement('input');
@@ -980,6 +991,56 @@ const renderToDos = (toDoArr) => {
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (renderToDos);
 
+
+/***/ }),
+
+/***/ "./src/assets/icons/dots-light.svg":
+/*!*****************************************!*\
+  !*** ./src/assets/icons/dots-light.svg ***!
+  \*****************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "dots-light.svg";
+
+/***/ }),
+
+/***/ "./src/assets/icons/dots.svg":
+/*!***********************************!*\
+  !*** ./src/assets/icons/dots.svg ***!
+  \***********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "dots.svg";
+
+/***/ }),
+
+/***/ "./src/assets/icons/tick.svg":
+/*!***********************************!*\
+  !*** ./src/assets/icons/tick.svg ***!
+  \***********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "tick.svg";
+
+/***/ }),
+
+/***/ "./src/assets/icons/trash-light.svg":
+/*!******************************************!*\
+  !*** ./src/assets/icons/trash-light.svg ***!
+  \******************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "trash-light.svg";
+
+/***/ }),
+
+/***/ "./src/assets/icons/trash.svg":
+/*!************************************!*\
+  !*** ./src/assets/icons/trash.svg ***!
+  \************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "trash.svg";
 
 /***/ })
 
@@ -1034,6 +1095,18 @@ const renderToDos = (toDoArr) => {
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
 /******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
@@ -1048,6 +1121,29 @@ const renderToDos = (toDoArr) => {
 /******/ 			}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
 /******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src;
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) {
+/******/ 					var i = scripts.length - 1;
+/******/ 					while (i > -1 && !scriptUrl) scriptUrl = scripts[i--].src;
+/******/ 				}
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/nonce */
