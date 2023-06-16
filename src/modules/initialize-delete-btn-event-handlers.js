7aa93fe203045930ceb48a815d535e37c327880b
Henok Kirubel <henokhm2@gmail.com>
2023-06-16 08:46:50 +0300

    Fix linting errors

--- src/modules/initialize-delete-btn-event-handlers.js
+++ src/modules/initialize-delete-btn-event-handlers.js
@@ -6,7 +6,6 @@ const initializeDeleteBtnEventHandlers = (toDoList) => {
   toDoListTarget.addEventListener('click', (e) => {
     if (e.target.classList.contains('to-do-item__delete-btn')) {
       const index = e.target.dataset.toDoIndex;
-      console.log(`Deleting to-do item with index ${index}`);
       toDoList.deleteToDoItem(index);
       renderToDos(toDoList.toDoList);
     }
