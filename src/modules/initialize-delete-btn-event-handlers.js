f5dbf9c93fc379dff703d200a72d3574f14e6e04
Henok Kirubel <henokhm2@gmail.com>
2023-06-16 22:27:48 +0300

    Add icons and icon-styles

--- src/modules/initialize-delete-btn-event-handlers.js
+++ src/modules/initialize-delete-btn-event-handlers.js
@@ -4,7 +4,7 @@ const toDoListTarget = document.querySelector('#to-do-list-target');
 
 const initializeDeleteBtnEventHandlers = (toDoList) => {
   toDoListTarget.addEventListener('click', (e) => {
-    if (e.target.classList.contains('to-do-item__delete-btn')) {
+    if (e.target.classList.contains('to-do-item__trash-icon')) {
       const index = e.target.dataset.toDoIndex;
       toDoList.deleteToDoItem(index);
       renderToDos(toDoList.toDoList);
--- src/modules/render-to-dos.js
+++ src/modules/render-to-dos.js
@@ -1,20 +1,31 @@
+import trashSrc from '../assets/icons/trash.svg';
+import dotsSrc from '../assets/icons/dots.svg';
+
 const generateToDoItemHTML = (toDoItem) => {
   const li = document.createElement('li');
   const checkBox = document.createElement('input');
   checkBox.setAttribute('type', 'checkbox');
   const description = document.createElement('input');
   const deleteBtn = document.createElement('button');
+  const trashIcon = document.createElement('img');
   const dragBtn = document.createElement('button');
+  const dragIcon = document.createElement('img');
 
   li.classList.add('to-do-item');
   description.classList.add('to-do-item__description');
   deleteBtn.classList.add('to-do-item__delete-btn');
+  trashIcon.classList.add('to-do-item__trash-icon');
+  dragBtn.classList.add('to-do-item__drag-btn');
+  dragIcon.classList.add('to-do-item__drag-icon');
 
   description.value = `${toDoItem.description}`;
-  deleteBtn.innerText = 'Delete';
+  trashIcon.src = trashSrc;
+  deleteBtn.appendChild(trashIcon);
+  dragIcon.src = dotsSrc;
+  dragBtn.appendChild(dragIcon);
 
   description.setAttribute('data-to-do-index', toDoItem.index);
-  deleteBtn.setAttribute('data-to-do-index', toDoItem.index);
+  trashIcon.setAttribute('data-to-do-index', toDoItem.index);
   dragBtn.setAttribute('data-to-do-index', toDoItem.index);
 
   li.appendChild(checkBox);
