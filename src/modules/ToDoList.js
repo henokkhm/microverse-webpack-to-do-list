13544d4276a9df7b1d8caf3bdc29fb995cd4a177
Henok Kirubel <henokhm2@gmail.com>
2023-06-16 07:49:10 +0300

    Add create to-do item method

--- src/modules/ToDoList.js
+++ src/modules/ToDoList.js
@@ -9,5 +9,17 @@ class ToDoList {
   get toDoList() {
     return this.#toDoList;
   }
+
+  createToDoItem(description) {
+    const newToDo = {
+      index: this.#toDoList.length,
+      description,
+      completed: false,
+    };
+
+    this.#toDoList.push(newToDo);
+    localStorage.setItem('to-do-list', JSON.stringify(this.#toDoList));
+    return newToDo;
+  }
 }
 export default ToDoList;
